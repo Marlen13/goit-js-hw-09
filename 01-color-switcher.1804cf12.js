6e50c0da3ac766f0c842214d2572cede56ae3330
Marlen13 <114424710+Marlen13@users.noreply.github.com>
2023-04-27 15:12:19 +0000

    Deploying to gh-pages from @ Marlen13/goit-js-hw-09@914655aa609cae09916e57dbc0ea2d03cb9030c8 🚀

---
+++ 01-color-switcher.1804cf12.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");t.addEventListener("click",(function(){e.classList.remove("disable"),clearInterva(null),console.dir("stop color")}))}();
+//# sourceMappingURL=01-color-switcher.1804cf12.js.map
---
+++ 01-color-switcher.dadc698f.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");t.addEventListener("click",(function(){e.classList.remove("disable"),clearInterva(null),console.dir("stop color")}));
+//# sourceMappingURL=01-color-switcher.dadc698f.js.map
