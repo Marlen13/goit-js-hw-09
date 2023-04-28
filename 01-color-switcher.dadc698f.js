34f3835a6e3e31cd4ccc3d739e7677572a93ac0e
Marlen13 <114424710+Marlen13@users.noreply.github.com>
2023-04-28 20:02:00 +0000

    Deploying to gh-pages from @ Marlen13/goit-js-hw-09@72c4c8bf59ab7e327a19e3fee4f9ca432a8887fe 🚀

--- 01-color-switcher.dadc698f.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");t.addEventListener("click",(function(){e.classList.remove("disable"),clearInterva(null),console.dir("stop color")}));
-//# sourceMappingURL=01-color-switcher.dadc698f.js.map
