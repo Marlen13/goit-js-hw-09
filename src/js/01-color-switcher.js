72c4c8bf59ab7e327a19e3fee4f9ca432a8887fe
Marlen13 <olenamarinova@gmail.com>
2023-04-28 23:01:26 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,27 +1,25 @@
+const body = document.querySelector('body');
 const btnStart = document.querySelector('[data-start]');
 const btnStop = document.querySelector('[data-stop]');
 const delay = 1000;
 let intervalId = null;
 
-// btnStart.addEventListener("click", onClick);
-btnStop.addEventListener('click', stopClick);
-function getRandomHexColor() {
-  console.dir(
-    `#${Math.floor(Math.random() * 16777215)
-      .toString(16)
-      .padStart(6, 0)}`
-  );
+btnStart.addEventListener('click', onClick);
+function onClick() {
+  btnStart.disabled = true;
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, delay);
 }
-// function onclick() {
-//   intervalId = setInterval(() => {
-//     function getRandomHexColor();
-//     btnStart.classList.add("disable");
-//     console.dir(`start not active`);
-//   }, delay);
-// }
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+btnStop.addEventListener('click', stopClick);
 
 function stopClick() {
-  btnStart.classList.remove('disable');
-  clearInterva(intervalId);
-  console.dir('stop color');
+  btnStart.disabled = false;
+  clearInterval(intervalId);
 }
