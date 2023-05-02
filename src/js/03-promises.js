f98ce16f49ab57fb0804b7072329e90a5d712e6b
Marlen13 <olenamarinova@gmail.com>
2023-05-02 09:46:59 +0300

    add library

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,9 @@
+import Notiflix from 'notiflix';
+
+const formPromise = document.querySelector('.form');
+formPromise.addEventListener('submit', createPromise);
+console.log(formPromise.event.target.elements.name[amount]);
+// if (formPromise.nextElementSibling.name[amount])
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
