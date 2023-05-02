b0c64af1a5fc00216962a6ec109c314326b67757
Marlen13 <114424710+Marlen13@users.noreply.github.com>
2023-05-02 05:04:55 +0000

    Deploying to gh-pages from @ Marlen13/goit-js-hw-09@1442a92ceadc4266aba011fbc9e5400c5e1a894e 🚀

--- 02-timer.4d1573ef.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("[data-start]");document.querySelector("[data-days]"),document.querySelector("[data-hours]"),document.querySelector("[data-minutes]"),document.querySelector("[data-seconds]");e.addEventListener("click",(function(){var e=new Date;console.log(e.toLocaleString())}))}();
-//# sourceMappingURL=02-timer.4d1573ef.js.map
--- 02-timer.a2eca600.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-start]");document.querySelector("[data-days]"),document.querySelector("[data-hours]"),document.querySelector("[data-minutes]"),document.querySelector("[data-seconds]");e.addEventListener("click",(function(){const e=new Date;console.log(e.toLocaleString())}));
-//# sourceMappingURL=02-timer.a2eca600.js.map
